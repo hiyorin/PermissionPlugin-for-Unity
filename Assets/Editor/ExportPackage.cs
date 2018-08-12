9473241da51c396dfdd61f9e874f170e304f9988
hiyorin <ponkotsu.hiyorin@gmail.com>
2018-08-12 22:52:50 +0900

    [fix] export script

--- Assets/Editor/ExportPackage.cs
+++ Assets/Editor/ExportPackage.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEditor;
 
 public class ExportPackage
@@ -7,10 +8,25 @@ public class ExportPackage
         "Assets/Plugins/PermissionPlugin",
     };
 
+    private const string ReadMe = "README.md";
+    private const string License = "LICENSE";
+
     [MenuItem("Assets/Export PermissionPlugin")]
     private static void Export()
     {
-        AssetDatabase.ExportPackage(Paths, "PermissionPlugin.unitypackage", ExportPackageOptions.Recurse);
+        string readmePath = Path.Combine(Application.dataPath, "Plugins/PermissionPlugin", ReadMe);
+        string licensePath = Path.Combine(Application.dataPath, "Plugins/PermissionPlugin", License);
+        File.Copy(Path.Combine(Application.dataPath, "..", ReadMe), readmePath);
+        File.Copy(Path.Combine(Application.dataPath, "..", License), licensePath);
+        AssetDatabase.Refresh();
+
+        AssetDatabase.ExportPackage(Paths, "PermissionPlugin-for-Unity.unitypackage", ExportPackageOptions.Recurse);
         Debug.Log("Export complete!");
+
+        File.Delete(readmePath);
+        File.Delete(licensePath);
+        File.Delete(readmePath + ".meta");
+        File.Delete(licensePath + ".meta");
+        AssetDatabase.Refresh();
     }
 }
